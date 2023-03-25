7b40eff6776ee27a07b0c72ed866025d0d05842f
stanybebe <tristan.e.whitehill@gmail.com>
2023-03-25 12:20:00 -0400

    clean up

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -29,73 +29,8 @@ void ofApp::setup(){
     lAudio.assign(bufferSize, 0.0);
     rAudio.assign(bufferSize, 0.0);
     
-//    drumA.setup(440, 0, sampleRate, .1, 50, 0, .1, bq_type_lowpass, 2000, .35);
-//
-//
-//    smoother = new Biquad(bq_type_lowpass, 5000/(float)sampleRate,2,-3);
-//    smootherB = new Biquad(bq_type_lowpass, 1000/(float)sampleRate,1,-3);
-//    smootherC = new Biquad(bq_type_lowpass, 10000/(float)sampleRate,1,-3);
-//    smootherD = new Biquad(bq_type_lowpass, 5000/(float)sampleRate,1,-3);
-//    smootherF = new Biquad(bq_type_lowpass, 1000/(float)sampleRate,1,-3);
-//    smootherG = new Biquad(bq_type_lowpass, 1/(float)sampleRate,1,-3);
-//    smootherH = new Biquad(bq_type_lowpass, 5000/(float)sampleRate,1,-3);
-//    smootherI = new Biquad(bq_type_lowpass, 5000/(float)sampleRate,1,-3);
-//    smootherJ = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherK = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherL = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherM = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherN = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherO = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherP = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherQ = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherR = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherS = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherT = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherU = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    smootherV = new Biquad(bq_type_lowpass, 500/(float)sampleRate,1,-3);
-//    drumASmoothSample = new Biquad(bq_type_lowpass, 1000/(float)sampleRate,1,-3);
-//    drumBSmoothSample = new Biquad(bq_type_lowpass, 1000/(float)sampleRate,1,-3);
-//    HighPass = new Biquad(bq_type_highpass, 20/(float)sampleRate,1,-3);
-//
-//    knobA.setup();
-//    knobA.value=1;
-//
     clock.setup();
-//
-//    knobC.setup();
-//    knobD.setup();
-//
-//    knobD.value=1;
-//    knobE.setup();
-//    //
-//    //
-//    //
-//    //    autoPan.setup(1, 0, sampleRate, 1);
-//    //    vib.setup(1, 0, sampleRate, 1);
-//    //    op1.setup(0,0,sampleRate, 1);
-//    //    ADSR = new adsr(0,10,0,30,sampleRate);
-//    env1.setup(110,10);
-//    env2.setup(110,130);
-//
-//
-//    //      padA.setup(440);
-//    plusStepA.setup(440);
-//    minusStepA.setup(440);
-//
-//    //
-//    //    d_in1 = new delayIn(10000, sampleRate);
-//    //    d_out1 = new delayOut(d_in1, 800);
-//    //
-//    //    XY.setup(100, 600);
-//    drumA.d_out1->setTime((.1));
-//
-//
-//    vS.setup();
-//
-//    //    for (int i=0; i<4; i++) {
-//    //        vS.vecSteps[i].toggle::setup();
-//    //    }
-//
+    
     SeqA.setup(sampleRate, 110, 10);
     SeqB.setup(sampleRate, 110, 410);
     
@@ -110,104 +45,8 @@ void ofApp::update(){
 void ofApp::draw(){
     
     ofBackground(34, 69, 78);
-//    plusStepA.draw(200, 240);
+    //    plusStepA.draw(200, 240);
     clock.draw(450, 50, 100, 1);
-//
-//    if (plusStepA.value ==1) {
-//        apadTrig= true;
-//    }
-//    if(apadTrig == true){
-//        aPadCounter=aPadCounter-1;
-//    }
-//    if(aPadCounter==0){
-//        aPadCounter = 20;
-//        apadTrig = false;
-//        stepi = stepi+ 1;
-//        if (stepi>8){
-//            stepi=8;
-//        }
-//
-//        vS.numSteps = stepi;
-//
-//        vS.setup();
-//
-//        for (int i=0; i<vS.numSteps; i++) {
-//            vS.vecSteps[i].toggle::setup();
-//        }
-//    }
-//    if (vS.numSteps != 0){
-//        minusStepA.draw(140,240);
-//        for (int i=0; i<vS.numSteps; i++) {
-//            vS.vecSteps[i].toggle::draw(30+(i*50), 380);
-//        }
-//    }
-//
-//    if(minusStepA.value==1){
-//        bpadTrig= true;
-//    }
-//    if(bpadTrig == true){
-//        bPadCounter=bPadCounter-1;
-//    }
-//    if(bPadCounter==0){
-//        bPadCounter=20;
-//        bpadTrig=false;
-//        stepi = stepi-1;
-//        vS.numSteps = stepi;
-//        if (vS.numSteps != 0){
-//            vS.setup();
-//            for (int i=0; i<vS.numSteps; i++) {
-//                vS.vecSteps[i].toggle::setup();
-//            }
-//        }
-//    }
-//
-//
-
-//
-//    if(t>=clock.value){
-//        t=0;
-//        drumA.ADSR->setGate(0);
-//        drumA.ADSR_2->setGate(0);
-//        currentStep = currentStep+1;
-//        if (vS.numSteps != 0){
-//            if (vS.vecSteps[currentStep].toggle::value == 1) {
-//                drumA.ADSR->setGate(1);
-//                drumA.ADSR_2->setGate(1);
-//            }
-//            else{
-//                drumA.ADSR->setGate(0);
-//                drumA.ADSR_2->setGate(0);
-//
-//            }
-//        }
-//    }
-//
-//    if(currentStep >= vS.numSteps){
-//        currentStep = 0;
-//    }
-//
-//
-//
-//
-//
-//    cout << currentStep<<endl;
-//    ofPushMatrix();
-//    env1.draw(10,1000,20,500,10,500);
-//    env2.draw(10,1000,20,500,10,500);
-//    knobA.draw(50, 50, 1000, 10);
-//    knobC.draw(50, 150, .8, 0);
-//    ofPopMatrix();
-//
-//    drumA.osc.setAmp(smootherD->process(knobC.value));
-//    //    padA.draw(10, 220);
-//
-//    //    if (padA.value == true){
-//    //
-//    //        drumA.ADSR->setGate(1);
-//    //        drumA.ADSR_2->setGate(1);
-//    //
-//    //    }else{drumA.ADSR->setGate(0); drumA.ADSR_2->setGate(0);}
-    
     SeqA.draw(200, 240,30,380, 50, 50, clock.value);
     SeqB.draw(200, 640,30,780, 50, 450, clock.value);
 }
@@ -261,45 +100,6 @@ void ofApp::newMidiMessage(ofxMidiMessage& msg) {
 void ofApp::audioOut(ofSoundBuffer & buffer){
     
     for (int i = 0; i<buffer.getNumFrames();i++){
-//        drumA.d_out1->setTime(smootherG->process(knobA.value));
-//        float currentS = 0;
-//
-//        currentS += drumASmoothSample->process(drumA.osc.getSine());
-//
-//        drumA.ADSR->aTime = smootherL->process(env1.attackV);
-//        drumA.ADSR->dTime = smootherM->process(env1.decayV);
-//        drumA.ADSR->sTime = smootherN->process(env1.susV);
-//        drumA.ADSR->rTime = smootherO->process(env1.relV);
-//
-//        drumA.ADSR_2->aTime = smootherH->process(env2.attackV);
-//        drumA.ADSR_2->dTime = smootherI->process(env2.decayV);
-//        drumA.ADSR_2->sTime = smootherJ->process(env2.susV);
-//        drumA.ADSR_2->rTime = smootherK->process(env2.relV);
-//
-//        float fRamp = ofMap(drumA.ADSR_2->getVal(), 0, 1, 390, 100);
-//        drumA.osc.setFreq(fRamp);
-//
-//
-//        drumA.ADSR -> process();
-//        drumA.ADSR_2 -> process();
-//
-//
-//
-//        currentS *= drumA.ADSR->getVal();
-        
-        //        filter->setFc(smoother->process(fRamp/float(sampleRate)+(clock.value/float(sampleRate))));
-        //        currentS = filter->process(currentS);
-//        //        currentS = HighPass->process(currentS);
-//        float dOutSample = drumA.d_out1->getSample();
-//
-//        drumA.d_in1->feed(currentS+(dOutSample*.7));
-        //        dOutSample = HighPass->process(dOutSample);
-        //        currentPanFrame = pans.pan(currentS + dOutSample, autoPan.getSine());
-        
-        
-//
-//        lAudio[i] =buffer[i*buffer.getNumChannels()+0] =currentS + dOutSample;
-//        rAudio[i] = buffer[i*buffer.getNumChannels()+1] =currentS + dOutSample;
         
         SeqA.processBlock(0);
         SeqB.processBlock(1);
--- src/ofApp.h
+++ src/ofApp.h
@@ -34,10 +34,6 @@ class voice : public ofBaseApp {
 public:
     
     
-    //      void setup(float freq_, double phase_, int sampleRate_, float amp_,
-    //                 float freq_2, double phase_2,float amp_2,int type, double Fc, double Q, double peakGainDB
-    //                 );
-    //
     void process();
     oscillator osc;
     oscillator op1;
@@ -244,10 +240,6 @@ public:
         
         
         currentS *= drumA.ADSR->getVal();
-        
-        //        filter->setFc(smoother->process(fRamp/float(sampleRate)+(clock.value/float(sampleRate))));
-        //        currentS = filter->process(currentS);
-        //        currentS = HighPass->process(currentS);
         float dOutSample = drumA.d_out1->getSample();
         
         drumA.d_in1->feed(currentS+(dOutSample*.7));
@@ -290,79 +282,8 @@ public:
     vector <float> lAudio;
     vector <float> rAudio;
     static const int numOsc = 12;
-//
-//    Steps vS,vSb;
-//    voice drumA, drumB;
     
-    //    oscillator osc;
-    //    oscillator oscB;
-    //    oscillator oscC;
-    //    oscillator oscD;
-    
-//
-//    vector <int> queueDaddy;
-    //    oscillator autoPan;
-    //    oscillator vib;
-    //    oscillator vib2;
-    //
-    //    oscillator op1;
-    //    Biquad* filter;
-//    Biquad* smoother;
-//    Biquad* smootherB;
-//    Biquad* smootherC;
-//    Biquad* smootherD;
-//    Biquad* smootherF;
-//    Biquad* smootherG;
-//    Biquad* smootherH;
-//    Biquad* smootherI;
-//    Biquad* smootherJ;
-//    Biquad* smootherK;
-//    Biquad* smootherL;
-//    Biquad* smootherM;
-//    Biquad* smootherN;
-//    Biquad* smootherO;
-//    Biquad* smootherP;
-//    Biquad* smootherQ;
-//    Biquad* smootherR;
-//    Biquad* smootherS;
-//    Biquad* smootherT;
-//    Biquad* smootherU;
-//    Biquad* smootherV;
-//    Biquad* drumASmoothSample;
-//    Biquad* drumBSmoothSample;
-//    Biquad* HighPass;
-    
-//    bool noteOn =false;
-//
     knob clock;
-//    pad padA;
-//    pad plusStepA;
-//    pad minusStepA;
-//    pad plusStepB;
-//    pad minusStepB;
-//
-//    Panner pans;
-    
-    //lerpVal smooth;
-//    stereoFrame currentPanFrame;
-//    int stepi;
-//    float t = 0;
-//    int aPadCounter = 20;
-//    bool apadTrig = false;
-//    int bPadCounter = 20;
-//    bool bpadTrig = false;
-//    bool bangA = false;
-//    int bangAcounter = 0;
-//    int currentStep = 0;
-//
-//    adsr* ADSR;
-//    envui env1;
-//    adsr* ADSR_2;
-//    envui env2;
-//    adsr* ADSR_3;
-//    envui env3;
-//    adsr* ADSR_4;
-//    envui env4;
     
     xy XY;
     
@@ -371,13 +292,7 @@ public:
     ofxMidiIn midiIn;
     std::vector<ofxMidiMessage> midiMessages;
     std::size_t maxMessages = 10; //< max number of messages to keep track of
-//
-//    delayIn* d_in1;
-//    delayOut* d_out1;
-//    delayIn* d_in2;
-//    delayOut* d_out2;
-//    int v , vo;
-//
+    
     sequencer SeqA, SeqB;
     
     double mtofarray[129]={0, 8.661957, 9.177024, 9.722718, 10.3, 10.913383, 11.562325, 12.25, 12.978271, 13.75, 14.567617, 15.433853, 16.351599, 17.323914, 18.354048, 19.445436, 20.601723, 21.826765, 23.124651, 24.5, 25.956543, 27.5, 29.135235, 30.867706, 32.703197, 34.647827, 36.708096, 38.890873, 41.203445, 43.65353, 46.249302, 49., 51.913086, 55., 58.27047, 61.735413, 65.406395, 69.295654, 73.416191, 77.781746, 82.406891, 87.30706, 92.498604, 97.998856, 103.826172, 110., 116.540939, 123.470825, 130.81279, 138.591309, 146.832382, 155.563492, 164.813782, 174.61412, 184.997208, 195.997711, 207.652344, 220., 233.081879, 246.94165, 261.62558, 277.182617,293.664764, 311.126984, 329.627563, 349.228241, 369.994415, 391.995422, 415.304688, 440., 466.163757, 493.883301, 523.25116, 554.365234, 587.329529, 622.253967, 659.255127, 698.456482, 739.988831, 783.990845, 830.609375, 880., 932.327515, 987.766602, 1046.502319, 1108.730469, 1174.659058, 1244.507935, 1318.510254, 1396.912964, 1479.977661, 1567.981689, 1661.21875, 1760., 1864.655029, 1975.533203, 2093.004639, 2217.460938, 2349.318115, 2489.015869, 2637.020508, 2793.825928, 2959.955322, 3135.963379, 3322.4375, 3520., 3729.31, 3951.066406, 4186.009277, 4434.921875, 4698.63623, 4978.031738, 5274.041016, 5587.651855, 5919.910645, 6271.926758, 6644.875, 7040., 7458.620117, 7902.132812, 8372.018555, 8869.84375, 9397.272461, 9956.063477, 10548.082031, 11175.303711, 11839.821289, 12543.853516, 13289.75};
